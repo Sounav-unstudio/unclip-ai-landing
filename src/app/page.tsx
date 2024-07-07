5f4b4bbedcc8ac77cc67166b21e4315e7e478dcf
GVMafra <gvmafra@outlook.com>
2024-07-07 14:18:01 -0300

    card test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import React from "react";
+import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
 
 export default function Home() {
   return (
@@ -22,22 +23,26 @@ export default function Home() {
               className="dark:invert"
               width={100}
               height={24}
-              priority
             />
           </a>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Card className="py-4">
+        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+          <p className="text-tiny uppercase font-bold">Daily Mix</p>
+          <small className="text-default-500">12 Tracks</small>
+          <h4 className="font-bold text-large">Frontend Radio</h4>
+        </CardHeader>
+        <CardBody className="overflow-visible py-2">
+          <Image
+            alt="Card background"
+            className="object-cover rounded-xl"
+            src="https://nextui.org/images/hero-card-complete.jpeg"
+            width={270}
+          />
+        </CardBody>
+      </Card>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
         <a
